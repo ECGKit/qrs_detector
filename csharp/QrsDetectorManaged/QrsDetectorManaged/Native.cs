4f42b720ac91af6c3aa56ea8d81cbf3082c85934
myd7349 <myd7349@gmail.com>
2020-08-01 17:08:06 +0800

    Add C# wrapper library

---
+++ csharp/QrsDetectorManaged/QrsDetectorManaged/Native.cs
@@ -0,0 +1,17 @@
+﻿namespace QrsDetectorManaged
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal static class Native
+    {
+        public const byte MARK_NO_QRS = 0;
+        public const byte MARK_QRS = 1;
+
+        [DllImport("qrs_detector.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DetectQrsPeaks")]
+        public static extern int DetectQrsPeaks([In] double[] signal, int length, [Out] byte[] positions, double samplingRate);
+    }
+}
+
+// References:
+// [Passing a char array from c# to c++ dll](https://stackoverflow.com/questions/38146181/passing-a-char-array-from-c-sharp-to-c-dll)
---
+++ csharp/QrsDetectorManaged/QrsDetectorManaged/QrsDetector.cs
@@ -0,0 +1,38 @@
+﻿namespace QrsDetectorManaged
+{
+    using System;
+    using System.Linq;
+
+    public static class QrsDetector
+    {
+        public static int[] Detect(double[] signal, double samplingRate)
+        {
+            if (signal == null)
+                throw new ArgumentNullException("signal");
+
+            if (signal.Length == 0)
+                throw new ArgumentException("signal");
+
+            if (samplingRate <= 1.0)
+                throw new ArgumentException("samplingRate");
+
+            var positions = new byte[signal.Length];
+            var count = Native.DetectQrsPeaks(signal, signal.Length, positions, samplingRate);
+            if (count > 0)
+            {
+                return positions.Select((r, i) => new { i, r })
+                    .Where(t => t.r == Native.MARK_QRS)
+                    .Select(t => t.i)
+                    .ToArray();
+            }
+            else
+            {
+                return new int[0];
+            }
+        }
+    }
+}
+
+// References:
+// [LINQ to find array indexes of a value](https://stackoverflow.com/questions/13291788/linq-to-find-array-indexes-of-a-value)
+// [Get indexes of all matching values from list using Linq](https://stackoverflow.com/questions/13055391/get-indexes-of-all-matching-values-from-list-using-linq)
